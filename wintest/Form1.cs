437b60e48aa5030f5159dbc177c1eebd00de8d35
OmarKallelPro <omar.kallel.pro@gmail.com>
2025-02-14 19:24:42 +0100

    Hello world!

--- wintest/Form1.cs
+++ wintest/Form1.cs
@@ -19,7 +19,7 @@ namespace wintest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "Hello";
+            textBox1.Text = "Hello world!";
         }
     }
 }
